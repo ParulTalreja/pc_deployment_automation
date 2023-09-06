68e6f56e019eb0d28d646c178c4f5fe719a88bd1
nutanix <anmol.garg@nutanix.com>
2023-09-06 11:20:49 -0400

    slack app changes

--- interacting_with_slack.py
+++ interacting_with_slack.py
@@ -2,9 +2,12 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from start_autotriage_deployment_bot import start_bot_analysis
 import ssl
+
+from time import sleep
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
-app = App(token="xoxb-2172428722-5834253288615-dWUKruIiNCeXJOGElVUBlAcV")
+app = App(token="xoxb-2172428722-5834253288615-yLtHilm9NQjody0UczowP8C1")
 
 # @app.event({
 #      "type": "message",
@@ -16,13 +19,76 @@ app = App(token="xoxb-2172428722-5834253288615-dWUKruIiNCeXJOGElVUBlAcV")
 #      say(text="Able to get messages", channel = "C05QMNCHTLN")
 
 @app.event("app_mention")
-def pc_mention_reply(event, say):
+def pc_mention_reply(event, say, client):
+    print("check-this")
+    thread_ts = event["ts"]
+    print("Thread", thread_ts)
     msg = event["text"][event["text"].find("> ")+3:-1]
     print(msg)
     result_analysis = start_bot_analysis(msg)
     print(result_analysis)
-    say(text = result_analysis, channel = "C05QMNCHTLN")
+    
+    client.chat_postMessage(text = result_analysis, channel = "C05QMNCHTLN", thread_ts = thread_ts)
+    
+    sleep(2)
+    
+    # Ask for feedback
 
+    blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"Hey there <@{event['user']}>! How was your experience with RDM?"
+            },
+        },
+        {
+            "type": "actions",
+            "block_id": "feedback_block",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": ":thumbsup: Great"
+                    },
+                    "style": "primary",
+                    "value": "great",
+                    "action_id": "review_great"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": ":thumbsdown: Bad"
+                    },
+                    "style": "danger",
+                    "value": "bad",
+                    "action_id": "review_bad"
+                }
+            ]
+        }
+    ]
+
+    client.chat_postEphemeral(channel="C05QMNCHTLN", thread_ts=thread_ts, user=event["user"], blocks=blocks)
+
+
+@app.action("review_great")
+def review_great(ack, body, client):
+    ack()
+    user = body["user"]["username"]
+    channel = body["channel"]["id"]
+    thread_ts = body["container"]["thread_ts"]
+    client.chat_postEphemeral(channel="C05QMNCHTLN", thread_ts=thread_ts, user=body["user"]["id"], text="Thank you for your feedback!")
+
+
+@app.action("review_bad")
+def review_bad(ack, body, client):
+    ack()
+    user = body["user"]["username"]
+    channel = body["channel"]["id"]
+    thread_ts = body["container"]["thread_ts"]
+    client.chat_postEphemeral(channel="C05QMNCHTLN", thread_ts=thread_ts, user=body["user"]["id"], text="Thank you for your feedback!")
+    
 if __name__ == "__main__":
-    SocketModeHandler(app, "xapp-1-A05QUUW297F-5847906674434-c5b52f13b5d321e91b17e4f7a66b97d12c066998c91a5509d8343f4dd6b13883").start()
-
+    SocketModeHandler(app, "xapp-1-A05QUUW297F-5851356677237-a3cf8634e69fa5ae6dde66172eab28c85a6071c37f5e43c1cf0447cd308c7c38").start()
