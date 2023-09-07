5b73743bd46df2570f18df7858e9287f9b98a178
nutanix <anmol.garg@nutanix.com>
2023-09-07 04:55:41 -0400

    changes

--- interacting_with_slack.py
+++ interacting_with_slack.py
@@ -22,7 +22,7 @@ app = App(token=os.environ["BUG_TRIAGE_BOT_BOT_TOKEN"])
 @app.event("app_mention")
 def pc_mention_reply(event, say, client):
     thread_ts = event["ts"]
-    client.chat_postMessage(text = "*Processing your query*", channel = "C05QMNCHTLN", thread_ts = thread_ts)
+    client.chat_postEphemeral(text = "_Processing your query_", channel = "C05QMNCHTLN", thread_ts = thread_ts, user=event["user"])
     print("Thread", thread_ts)
     msg = event["text"][event["text"].find("> ")+3:-1]
     print(msg)
